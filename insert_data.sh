0a3c8a960160b17367929e6fe105cb572baf655a
abdhaseeeb <114016113+abdhaseeeb@users.noreply.github.com>
2024-11-19 00:01:38 -0600

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Clear existing data
+$PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY"
+
+# Read games.csv and populate the database
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    # Insert winner and opponent into teams table
+    for TEAM in "$WINNER" "$OPPONENT"
+    do
+      TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$TEAM'")
+      if [[ -z $TEAM_ID ]]
+      then
+        $PSQL "INSERT INTO teams(name) VALUES('$TEAM')"
+      fi
+    done
+
+    # Get team IDs
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    # Insert game into games table
+    $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)"
+  fi
+done
